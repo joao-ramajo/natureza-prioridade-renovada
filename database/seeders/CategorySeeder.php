2f5834e10adfa667b170cfbc7085d311c1c5eb4f
LacamJC <joaoramajo744@gmail.com>
2025-07-02 22:10:53 -0300

    chore: adição de uma classe Seeder para a migration categories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            ['name' => 'Ecológico', 'created_at' => now()],
+            ['name' => 'Reciclável', 'created_at' => now()],
+            ['name' => 'Não reciclável', 'created_at' => now()],
+            ['name' => 'Perigoso', 'created_at' => now()],
+            ['name' => 'Eletrônico', 'created_at' => now()],
+            ['name' => 'Hospitalar', 'created_at' => now()],
+            ['name' => 'Radioativo', 'created_at' => now()],
+            ['name' => 'Construção Civil', 'created_at' => now()],
+            ['name' => 'Pneus', 'created_at' => now()],
+            ['name' => 'Óleo usado', 'created_at' => now()],
+        ]);
+    }
+}

d4d821de8ea314d62824b957d253bc829b8f4e5f
LacamJC <joaoramajo744@gmail.com>
2025-07-14 02:28:16 -0300

    chore: nova estrutura de pastas, separadas por responsabilidades e assuntos gerais

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,42 +1,29 @@
 @extends('layouts.main_layout')
+@section('head')
+    <link rel="stylesheet" href="{{ asset('assets/css/home/hero/hero.css') }}">
+@endsection
 
 @section('content')
     <x-layout.header></x-layout.header>
-    {{-- <section id="collectionPoints" class="container py-5">
 
-        <a href="{{ route('csv-export') }}">Exportar</a>
-
-        <h2 class="mb-4 text-center">Pontos de Coleta</h2>
-
-        <form method="GET" action="{{ route('home') }}" class="mb-4 d-flex justify-content-center">
-            <select name="category" class="form-select w-auto" onchange="this.form.submit()">
-                <option value="">Todas as categorias</option>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->id }}" {{ request('category') == $category->id ? 'selected' : '' }}>
-                        {{ $category->name }}
-                    </option>
-                @endforeach
-            </select>
-        </form>
-
-        <div class="row g-4">
-            @forelse ($points as $point)
-                <div class="col-lg-6 col-md-8 col-sm-12 mx-auto">
-                    <a href="{{ route('collection_point.view', ['id' => Crypt::encrypt($point->id)]) }}"><x-collection-point.card
-                            :point="$point" /></a>
+    <section class="hero">
+        <div class="container">
+            <div class="row align-items-center hero__row">
+                <div class="col-md-6 hero__content">
+                    <h1 class="hero__title">Natureza Prioridade Renovada</h1>
+                    <p class="hero__text mb-5">Nós, da NPR, estamos empenhados em contribuir para o combate ao descarte
+                        irregular
+                        de resíduos em nossa cidade. Oferecemos pontos de coleta estrategicamente localizados, um mapa
+                        informativo que destaca esses pontos e uma ampla quantidade de informações sobre práticas
+                        sustentáveis. Acreditamos que, juntos, podemos fazer a diferença e ajudar a preservar o meio
+                        ambiente.</p>
+                    <a href="/contato" class="hero__cta">Fale Conosco</a>
                 </div>
-            @empty
-                <div class="col-12">
-                    <p class="text-center text-muted">Nenhum ponto de coleta encontrado para essa categoria.</p>
+
+                <div class="col-md-6 hero__image-wrapper">
+                    <div class="hero__image"></div>
                 </div>
-            @endforelse
+            </div>
         </div>
-
-        <div class="d-flex justify-content-center mt-4">
-            {{ $points->links() }}
-        </div>
-
-    </section> --}}
-
-    @include('home.hero')
+    </section>
 @endsection

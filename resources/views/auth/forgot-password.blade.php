fd2796feddcb81a1c393413d61b2d8f76e55aa0f
LacamJC <joaoramajo744@gmail.com>
2025-07-15 17:18:00 -0300

    feat: implementando nova view para recuperação de senha `forgot-password.blade.php`

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,13 +1,46 @@
 @extends('layouts.main_layout')
 
-@section('content')
-    <x-form.form title='Esqueci minha senha' btnLabel='Enviar' route='password.email' method='POST'>
+@section('head')
+    <link rel="stylesheet" href="{{ asset('assets/css/login/main.css') }}">
+@endsection
 
-        <x-form.input-field label="Email" type="email" name="email" value="{{ old('email') }}" />
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-    </x-form.form>
+@section('content')
+    <div class="logo">
+        <img src="{{ asset('assets/img/logosimp.svg') }}" alt="Logo da NPR">
+    </div>
+    <main class="main_content row w-100">
+
+        <div class="col col-md-6 left-side">
+            <form action="{{ route('password.confirm') }}" method="POST" class="form">
+                @csrf
+                <h2 class="form-subtitle">Recuperação de conta</h2>
+                <p class="text-secondary text-center">
+                    Um email será enviado ao endereço preenchido abaixo com as orientações para recuperação de conta.
+                </p>
+
+                <div class="form-input">
+                    <label for="email" class="form-input__label">Coloque seu email</label>
+                    <input type="email" name="email" id="email" class="form-input__input">
+
+                    <div class="row">
+                     
+                        <div class="col">
+                            <div class="form-input__forgot-password">
+                                <a href="{{ route('login') }}">Fazer login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="form__submit-button">Continuar</button>
+
+
+
+            </form>
+        </div>
+
+        <div class="col col-md-6 right-side">
+
+        </div>
+
+    </main>
 @endsection

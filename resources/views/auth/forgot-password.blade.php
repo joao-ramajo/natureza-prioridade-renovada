58ccad5d3bacba8d4bdfc86dbc859271fe88be27
LacamJC <joaoramajo744@gmail.com>
2025-07-15 17:22:58 -0300

    chore: adicionando mensagem para verificar o span em `forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -11,7 +11,7 @@
     <main class="main_content row w-100">
 
         <div class="col col-md-6 left-side">
-            <form action="{{ route('password.confirm') }}" method="POST" class="form">
+            <form action="{{ route('password.email') }}" method="POST" class="form">
                 @csrf
                 <h2 class="form-subtitle">Recuperação de conta</h2>
                 <p class="text-secondary text-center">
@@ -23,7 +23,7 @@
                     <input type="email" name="email" id="email" class="form-input__input">
 
                     <div class="row">
-                     
+
                         <div class="col">
                             <div class="form-input__forgot-password">
                                 <a href="{{ route('login') }}">Fazer login</a>
@@ -32,8 +32,12 @@
                     </div>
                 </div>
                 <button type="submit" class="form__submit-button">Continuar</button>
-
-
+                <p class="text-secondary mt-3" style="font-size: .9rem">Caso já tenha solicitado, verifique sua caixa de spam.</p>
+                @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
             </form>
         </div>
